6b0ced9611bbc3a0e38f0b1a745d566304956658
alienx456 <estebanelias27dev@gmail.com>
2021-07-02 15:32:13 -0500

    updated backend

--- IAC/backend.tf
+++ IAC/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket  = "terraform-monitor-provide-states-files"
-    key     = "noiseLevel.tfstate"
+    key     = "logstash.tfstate"
     region  = "us-east-1"
   }
 }
