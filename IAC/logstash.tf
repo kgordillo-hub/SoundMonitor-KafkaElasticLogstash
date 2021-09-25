2c71cf42a6e916a63abaff875255c6a4393ff8ca
alienx456 <estebanelias27dev@gmail.com>
2021-09-25 14:33:37 -0500

    update version

--- IAC/logstash.tf
+++ IAC/logstash.tf
@@ -1,5 +1,5 @@
 module "logstash" {
-  source = "git::https://github.com/AlienX456/SoundMonitor-IAC-Infrastructure-Common.git//use-cases/ecs_logstash_elastic_services?ref=3.0.9-rc"
+  source = "git::https://github.com/AlienX456/SoundMonitor-IAC-Infrastructure-Common.git//use-cases/ecs_logstash_elastic_services?ref=3.0.10-rc"
 
   /*USER VARS*/
   cpu= "256"
